4f1e2a019f4a9d89bf542844946df8e40a485548
github99github <dasf@tmpbox.net>
2021-01-22 20:52:07 +0300

    refresh

--- gulpJS/this project gulp/gulpfile.js
+++ gulpJS/this project gulp/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var prefix = require('gulp-autoprefixer');
 
 
 // your first task 
@@ -20,8 +21,17 @@ var concat = require('gulp-concat');
 //});
 
 
-gulp.task('js', function(){
-    return gulp.src('project/*.js')
-            .pipe(concat('all.js'))
-            .pipe(gulp.dest('dist/js'))
+//gulp.task('js', function(){
+//    return gulp.src('project/*.js')
+//            .pipe(concat('all.js'))
+//            .pipe(gulp.dest('dist/js'))
+//});
+
+
+//autoprefixer
+gulp.task('css', function(){
+    return gulp.src('project/*.css')
+            .pipe(prefix('last 2 versions'))
+            .pipe(concat('main.css'))
+            .pipe(gulp.dest('dist/css'))
 });
